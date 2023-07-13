8e24f8746ba95a5f63cc97e002425c7815b825eb
shyzik93 <shyzik93@mail.ru>
2023-07-13 08:29:15 +0300

    fix draft of adding server view

--- note/urls.py
+++ note/urls.py
@@ -7,6 +7,7 @@ from note.views import (
 )
 
 urlpatterns = [
+    path('storage_services/<str:pk>', NoteStorageServiceListView.as_view(), name='note_service_storage_add_or_edit'),
     path('storage_services', NoteStorageServiceListView.as_view(), name='note_service_storage_list'),
     path('<str:quoted_title>.md', NoteEditorView.as_view(), name='note_editor'),
     path('', NoteListView.as_view(), name='note_list'),
