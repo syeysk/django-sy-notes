52574163d6051d1d0c3830c1503bb8a9845b9cc0
shyzik93 <shyzik93@mail.ru>
2023-09-16 17:14:43 +0300

    fix urls

--- server/urls_api.py
+++ server/urls_api.py
@@ -1,15 +1,8 @@
 from django.urls import path, include
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
 
 
 urlpatterns = [
-    path('v1/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('v1/note/', include('note.urls_api')),
-    path('v1/linker/', include('django_sy_framework.linker.urls')),
+    path('v1/linker/', include('django_sy_framework.linker.urls_api')),
+    path('', include('django_sy_framework.base.urls_api')),
 ]
