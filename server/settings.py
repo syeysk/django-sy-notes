6f240c83200cd9ea9ca84da8a0d1c90a78716ba4
shyzik93 <shyzik93@mail.ru>
2023-09-21 22:12:35 +0300

    more safe

--- server/settings.py
+++ server/settings.py
@@ -87,7 +87,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': BASE_DIR / 'debug.log',
+            'filename': BASE_DIR / '.debug.log',
         },
     },
     'loggers': {
